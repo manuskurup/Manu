458fd56f1a13ba8b747f80c26b26a3a9be9c257a
vishnukumarps <vishnukumar29518@gmail.com>
2020-05-28 08:34:27 +0530

    OOp1

---
+++ OOPS.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Manu
+{
+
+
+    class Employee
+    {
+        public int EmpId { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+
+
+    }
+    public class Student
+    {
+      
+       public  string Name;
+        int M1, M2;
+        
+        public int Age { get; set; }
+
+        private int RollNo;
+        public void SetRollNo(int RollNo)
+        {
+            this.RollNo = RollNo;
+
+        }
+        public int GetRollNo()
+        {
+            return this.RollNo;
+        }
+
+
+
+
+
+        void Total()//function
+        {
+
+        }
+    }
+
+    class OOPS
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student();
+            s1.Name = "vishnu";
+            s1.Age = 12;
+            Console.WriteLine(s1.Age);
+
+            s1.SetRollNo(2);
+            Console.WriteLine(s1.GetRollNo());
+        }
+
+    }
+}
