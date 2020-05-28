e5674335ab0579a4002253a62fc8e34d11323322
vishnukumarps <vishnukumar29518@gmail.com>
2020-05-28 07:42:35 +0530

    Array

---
+++ Arrays.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Manu
+{
+    class Arrays
+    {
+        static void Main(string[] args)
+        {
+
+            int[] a = new int[10];
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = i;
+            }
+
+            Console.WriteLine("output");
+            foreach (int item in a)
+            {
+                Console.WriteLine(item);
+               
+
+            }
+            Console.ReadLine();
+                
+        }
+    }
+}
