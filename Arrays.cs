f1bbe24a3f90df94600b68cf42a168b8701cf0d6
vishnukumarps <vishnukumar29518@gmail.com>
2020-05-28 07:56:33 +0530

    2d Array

--- Arrays.cs
+++ Arrays.cs
@@ -8,6 +8,31 @@ namespace Manu
     {
         static void Main(string[] args)
         {
+            int [,]b = new int[2,2];
+            for (int i = 0; i <2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    b[i, j] = j;
+                }
+            }
+
+
+            Console.WriteLine("output1");
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.Write(b[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+
+
+            Console.WriteLine("________________________________");
+
 
             int[] a = new int[10];
             for (int i = 0; i < a.Length; i++)
@@ -15,7 +40,7 @@ namespace Manu
                 a[i] = i;
             }
 
-            Console.WriteLine("output");
+            Console.WriteLine("output2");
             foreach (int item in a)
             {
                 Console.WriteLine(item);
