60ced5c6335ec31a17bd1ce479691be1a0b30f8e
vishnukumarps <vishnukumar29518@gmail.com>
2020-05-29 08:07:15 +0530

    Day_5

---
+++ Functions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Manu
+{
+    public class Emp1
+    {
+      public  void SameName()
+        {
+            Console.WriteLine("Same Name");
+        }
+        public void SameName(string pram1)
+        {
+            Console.WriteLine(pram1);
+        }
+
+    }
+
+    class Functions
+    {
+        static void Main(string[] args)
+        {
+            Emp1 o = new Emp1();
+            o.SameName();
+            o.SameName("Manu");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Test.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Manu
+{
+    class Sample
+    {
+       public  Sample()//Default
+        {
+            Console.WriteLine("Good morning");
+        }
+        public Sample(int x)
+        {
+            Console.WriteLine(x);
+        }
+
+       public void hello()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+
+
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            Sample obj = new Sample(2);
+            Sample obj2 = new Sample();
+            obj.hello();
+            Console.ReadKey();
+        }
+
+    }
+}
